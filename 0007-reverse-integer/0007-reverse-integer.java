78993105dcd35e784e549ddfe5dc7e2aeeb3a412
omsri8032 <112086358+omsri8032@users.noreply.github.com>
2024-08-05 18:17:03 +0530

    Time: 1 ms (78.46%), Space: 40.7 MB (50.09%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int reverse(int x) {
+        int N=Math.abs(x);
+        int n=Math.abs(x);
+int count=0;
+double rem;
+double rn=0;
+    while(n>0)
+{
+rem=n%10;
+n=n/10;
+count++;
+}
+for(int i=count;i>0;i--)
+{
+    rem=N%10;
+    N=N/10;
+    rn=rem*(Math.pow(10, i-1))+rn;
+    
+    
+}
+double  nrn= -1* (int)rn;
+
+if(nrn<=Math.pow(-2,31)||rn>=(Math.pow(2,31)-1))
+return(0);
+else if(x>0)
+return((int)rn); 
+else
+{
+return((int)nrn);
+}
+    }
+}
